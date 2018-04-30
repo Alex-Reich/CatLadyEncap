d0ff4aa55243b58cf825e3a86acc0aa5250eea31
Alex Reich <atrain.reich@gmail.com>
2018-04-30 13:50:08 -0600

    Initial Commit

---
+++ Assets/app.js
@@ -0,0 +1,35 @@
+// Private: cat draw cat = newCat active cat // Public: makeCat Pet catNip
+
+//Public
+function makeCat() {
+
+}
+
+}
+
+function pet() {
+    tolerance++
+}
+
+function catNip() {
+    tolerance = 0
+}
+
+
+// Private
+function catApp() {
+    function drawCat() {
+
+    }
+
+    function Cat(name, imgLink) {
+        this.name = name
+        this.catPic = imgLink
+        this.tolerance = 5
+    }
+
+    var Felix = new Cat("Felix", "//placehold.it//250x250") {
+    }
+
+}
+
